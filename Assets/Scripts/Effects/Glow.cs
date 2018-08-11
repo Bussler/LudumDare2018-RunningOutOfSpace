4554272d2b612143334709e8aaf3ecd5e81dc5f7
Luca <hohmann.luca@gmx.de>
2018-08-11 15:04:36 +0200

    Added first meteor, and also made a glow script, which will increase and decrease the glow amount

---
+++ Assets/Scripts/Effects/Glow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour {
+    private bool up = false;
+    private Color actualColor;
+    public Color destColor;
+
+    private Color startColor;
+
+    private float ColorDifference = 1.5f;
+    
+    private Material a;
+	// Use this for initialization
+	void Start () {
+	    a = GetComponent<Renderer>().material;
+	    actualColor = a.GetColor("_EmissionColor");
+	    startColor = a.GetColor("_EmissionColor");
+	    destColor = startColor;
+	    destColor.r -= 20f;
+	    destColor.g -= 20f;
+	    destColor.b -= 20f;
+	}
+    
+	// Update is called once per frame
+	void Update () {
+	    Debug.Log(up);
+	    if (up) {
+	        actualColor.r += ColorDifference*Time.deltaTime;
+	        actualColor.g += ColorDifference*Time.deltaTime;
+	        actualColor.b += ColorDifference*Time.deltaTime;
+	        a.SetColor("_EmissionColor", actualColor);
+	        if (actualColor.r > startColor.r) {
+	            up = false;
+	        }
+	        
+	    } else {
+	        Debug.Log(actualColor);
+	        actualColor.r -= ColorDifference*Time.deltaTime;
+	        actualColor.g -= ColorDifference*Time.deltaTime;
+	        actualColor.b -= ColorDifference * Time.deltaTime;
+	        a.SetColor("_EmissionColor", actualColor);
+	        if (actualColor.r < 0 && actualColor.g < 0 && actualColor.b < 0) {
+	            up = true;
+	        }
+	        
+	    }
+	}
+}
